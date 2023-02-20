962d594c89bc5eb9b31a46398167b2bc4f62461c
YoungwooKim09 <youngwoo9699@gmail.com>
2023-02-20 13:52:22 +0900

    Feat: 230220_백준_문제풀이

---
+++ Baekjoon/javascript/2231.js
@@ -0,0 +1,58 @@
+const readline = require("readline");
+const { cachedDataVersionTag } = require("v8");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let N = null;
+
+rl.on("line", function (line) {
+  N = parseInt(line);
+  rl.close();
+}).on("close", function () {
+  let M = 0;
+  //   // for문
+  //   for (let i = 1; i < N; i++) {
+  //     let candidateValue = i;
+  //     let stringValue = candidateValue.toString();
+
+  //     let sum = candidateValue;
+
+  //     for (let j = 0; j < stringValue.length; j++) {
+  //       sum += parseInt(stringValue[j]);
+  //     }
+  //     if (sum == N) {
+  //       M = candidateValue;
+  //       break;
+  //     }
+  //   }
+
+  // while문
+  let candidateValue = 1;
+
+  while (true) {
+    // sum > N은 틀린 조건 : 89 -> 106, 90 -> 99
+    if (candidateValue > N) {
+      break;
+    }
+
+    let sum = candidateValue;
+    let stringValue = candidateValue.toString();
+
+    for (let i = 0; i < stringValue.length; i++) {
+      sum += parseInt(stringValue[i]);
+    }
+
+    if (sum == N) {
+      M = candidateValue;
+      break;
+    }
+
+    candidateValue++;
+  }
+
+  console.log(M);
+  process.exit();
+});
